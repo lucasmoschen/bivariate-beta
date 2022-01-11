b8035f0466a7d23b1bfa0bdf37d25ccb1116b2bd
lucasmoschen <lucas.machadomoschen@gmail.com>
2022-01-11 20:08:21 -0300

    Added gitignore and environment

---
+++ scripts/parameter_estimation.py
@@ -0,0 +1,12 @@
+#usr/bin/env python3
+"""Parameter estimation of Bivariate Beta form Olkin and Trikalinos (2015)
+
+Author: Lucas Moschen
+
+This script is a support for the (unpublished) paper "On a bivariate beta" 
+from Lucas Machado Moschen and Luiz Max Carvalho. It allows the user to estimate 
+the parameter alpha as explained in the notes. 
+
+This script requires that `numpy` be installed within the Python 
+environment you are running. 
+"""
