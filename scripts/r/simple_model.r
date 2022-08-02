f98565e806d05923439b090329c4846226ca97ad
lucasmoschen <lucas.machadomoschen@gmail.com>
2022-08-01 22:22:05 -0300

    More insights about a simple model

--- scripts/r/simple_model.r
+++ scripts/r/simple_model.r
@@ -5,14 +5,15 @@ library("dplyr")
 options(mc.cores = parallel::detectCores())
 
 true_alpha <- c(2.5, 0.1, 1.9, 1.7)
-n <- 100
-U1 <- rbeta(n=n, shape1=2.5, shape2=1.6)
+n <- 2
+U1 <- rbeta(n=n, shape1=2.5, shape2=0.1)
 U2 <- rbeta(n=n, shape1=1.9, shape2=1.7)
+X <- U1 + U2
 
-data <- list(x = U1 + U2,
+data <- list(x = X,
              n = n,
              a = 2.5,
-             b = 1.6,
+             b = 0.1,
              c = 1.9,
              d = 1.7)
 
@@ -22,5 +23,5 @@ fit <- sampling(mod, data=data, warmup=1000, iter=2000)
 # Optional visualizations
 
 ## Shinystan visualization
-#shiny_obj <- as.shinystan(fit)
-#launch_shinystan(shiny_obj)
+shiny_obj <- as.shinystan(fit)
+launch_shinystan(shiny_obj)
