7ab41295d88cb36fcbba9eb251268ecb08697a88
Lucas Moschen <lucasmoschen@github.com>
2022-09-19 10:03:30 -0300

    Including sbc in .py file

--- scripts/python/experiments.py
+++ scripts/python/experiments.py
@@ -19,6 +19,7 @@ from time import time
 import os
 import json
 from __init__ import ROOT_DIR
+from cmdstanpy import CmdStanModel
 
 def starting_experiment(true_alpha, sample_size, monte_carlo_size, bootstrap_size, seed):
     """
@@ -202,15 +203,57 @@ def experiment_logitnormal(mu, sigma, sample_size, monte_carlo_size, seed):
 
         saving_document_2(filename, bias_new, mse_new, mape_new)
 
+def simulated_based_calibration(a, b, c, n, L=63, N=1000, seed=831290):
+
+    a = a*np.ones(4)
+    b = b*np.ones(4)
+    c = 0.0
+
+    data = {'n': n, 'a': a, 'b': b, 'c': c}
+
+    rho_values = []
+    rng = np.random.RandomState(seed)
+
+    stanfile = os.path.join('..', '..', 'scripts', 'stan', 'bivariate-beta-model-v3.stan')
+    model = CmdStanModel(stan_file=stanfile, cpp_options={'STAN_THREADS': True})
+
+    for _ in trange(2*N):
+        # Data 
+        true_alpha = rng.gamma(shape=a, scale=1/b, size=4) + c
+        U = rng.dirichlet(true_alpha, size=n)
+        if U[U<np.finfo(np.float64).eps].shape[0] > 0:
+            continue
+        X = U[:,0] + U[:,1]
+        Y = U[:,0] + U[:,2]
+        XY = np.column_stack([X,Y])
+        data['xy'] = XY
+
+        model_fit = model.sample(data=data, iter_warmup=1000, iter_sampling=1000, chains=1, adapt_delta=0.9,
+                                 show_progress=False, show_console=False)
+        alpha_estimates = (model_fit.stan_variables()['alpha'])[rng.choice(range(1000), size=L, replace=False)]
+        rho = np.sum(alpha_estimates > true_alpha, axis=0)
+        rho_values.append({'rho': rho.tolist(), 
+                           'diagnose': model_fit.diagnose(),
+                           'true_alpha': true_alpha.tolist(),
+                           'XY': XY.tolist(),
+                           'U': U.tolist()})
+
+    folder = os.path.join(ROOT_DIR, '..', '..', 'experiments', 'sbc')
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+    name = "sbc_{}_{}_{}_{}_{}_{}_{}.json".format(a[0], b[0], c, n, L, N, seed)
+    with open(os.path.join(folder, name), "w") as final:
+        json.dump(rho_values, final)
+
 if __name__ == '__main__':
 
-    monte_carlo_size = 1000
-    bootstrap_size = 500
-    seed = 617836829321
+    #monte_carlo_size = 1000
+    #bootstrap_size = 500
+    #seed = 617836829321
 
-    true_alpha = np.array([1,1,1,1])
-    experiment_bivbeta(true_alpha, 50, monte_carlo_size, bootstrap_size, seed)
-    experiment_bivbeta(true_alpha, 200, monte_carlo_size, bootstrap_size, seed)
+    #true_alpha = np.array([1,1,1,1])
+    #experiment_bivbeta(true_alpha, 50, monte_carlo_size, bootstrap_size, seed)
+    #experiment_bivbeta(true_alpha, 200, monte_carlo_size, bootstrap_size, seed)
 
     # true_alpha = np.array([2,7,3,1])
     # experiment_bivbeta(true_alpha, 50, monte_carlo_size, bootstrap_size, seed)
@@ -228,6 +271,15 @@ if __name__ == '__main__':
     # mu = np.array([-1.0, -1.0])
     # sigma = np.array([[2.25, -1.2], [-1.2, 1]])
     # experiment_logitnormal(mu, sigma, n, monte_carlo_size, seed)
+
+    a = 1
+    b = 1
+    c = 0
+    n = 50
+    L = 63
+    N = 1000
+    seed = 831290
+    simulated_based_calibration(a, b, c, n, L, N, seed)
 
 
 
